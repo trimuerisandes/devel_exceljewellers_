c5974377dcceddd9a0d2927031e9762eff3c8f83
trimuerisandes <trimuerisandes@gmail.com>
2022-01-11 10:58:38 +0700

    deliver to client

--- storage/framework/views/32118dc57a32e3b25ed739896825b88bddaa5cfb.php
+++ storage/framework/views/32118dc57a32e3b25ed739896825b88bddaa5cfb.php
@@ -1,11 +1,26 @@
 <hr>
 
-<div>
+<!--<div>
     <p class="text-center mt-5" style="margin-bottom: 0px;">NEWSLETTER</p>
     <div class="text-center mb-5"><img src="<?php echo e(asset('storage/image/icons/home_line.png')); ?>"></div>
     <p class="text-center mb-2">We promise only send the good things</p>
     <div class="text-center">
-        <form class="d-inline-flex">
+        <form class="d-inline-flex" id="newsletter-form"
+              action="https://www.snapretail.com/retailer/ConsumerSignupForm.aspx/Save?DataCollectionType=EmbeddedWebForm"
+              method="post" target="_blank">
+            <input id="Token" name="Token" type="hidden"
+                   value="iOPgyySGcFGjb4nqD0MKeg"/>
+
+            <input id="RedirectUrl" name="RedirectUrl" type="hidden" value=""/>
+            <div class="collectEmailFormGroup">
+                <div class="label-error-group">
+                            <span class="field-validation-valid validation-message" data-valmsg-for="EmailAddress"
+                                  data-valmsg-replace="true"></span>
+                </div>
+                <input class="form-control form-control-sm" data-val="true" data-val-required="Required" id="" maxlength="255"
+                       name="EmailAddress" placeholder="Your Email" type="email" value=""/>
+            </div>
+
             <div style=" display: -ms-flexbox;
   display: flex;
   -ms-flex-wrap: wrap;
@@ -16,7 +31,7 @@
                     <input type="text" class="form-control form-control-sm" placeholder="Your Email">
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control ml-3 form-control-sm" placeholder="Your Full Name">
+                    <input type="text" class="form-control form-control-sm" placeholder="Your Full Name">
                 </div>
                 <div class="collectEmailFormGroup collectEmailFormActions mb-5">
                     <button type="submit" class="newsletter-btn"><i class="fa fa-send"></i></button>
@@ -24,7 +39,7 @@
             </div>
         </form>
     </div>
-</div>
+</div>-->
 <div class="footer-container mt-5 mb-5">
     <div class="footer-inner mr-5">
         <div class="footer-open mr-5">
@@ -53,43 +68,43 @@
         </div>
     </div>
     <hr class="phone-hr">
-<!--    <div class="footer-inner">
-&lt;!&ndash;        <div class="footer-title"><b>GET OUR NEWSLETTER</b><span class="foot-icon icon-chevron-down"></span></div>
-        <div class="footer-open">
-            <form id="newsletter-form"
-                  action="https://www.snapretail.com/retailer/ConsumerSignupForm.aspx/Save?DataCollectionType=EmbeddedWebForm"
-                  method="post" target="_blank"><input id="Token" name="Token" type="hidden"
-                                                       value="iOPgyySGcFGjb4nqD0MKeg"/>
+    <div class="footer-inner" style="margin-right: 20px;">
+            <div class="footer-title"><b>GET OUR NEWSLETTER</b><span class="foot-icon icon-chevron-down"></span></div>
+            <div class="footer-open">
+                <form id="newsletter-form"
+                      action="https://www.snapretail.com/retailer/ConsumerSignupForm.aspx/Save?DataCollectionType=EmbeddedWebForm"
+                      method="post" target="_blank"><input id="Token" name="Token" type="hidden"
+                                                           value="iOPgyySGcFGjb4nqD0MKeg"/>
 
-                <input id="RedirectUrl" name="RedirectUrl" type="hidden" value=""/>
+                    <input id="RedirectUrl" name="RedirectUrl" type="hidden" value=""/>
 
-                <div class="collectEmailFormGroup">
-                    <div class="label-error-group">
-                        <span class="field-validation-valid validation-message" data-valmsg-for="EmailAddress"
-                              data-valmsg-replace="true"></span>
+                    <div class="collectEmailFormGroup">
+                        <div class="label-error-group">
+                            <span class="field-validation-valid validation-message" data-valmsg-for="EmailAddress"
+                                  data-valmsg-replace="true"></span>
+                        </div>
+                        <input class="form-control form-control-sm" data-val="true" data-val-required="Required" id="EmailAddress" maxlength="255"
+                               name="EmailAddress" placeholder="Your Email" type="email" value=""/>
                     </div>
-                    <input data-val="true" data-val-required="Required" id="EmailAddress" maxlength="255"
-                           name="EmailAddress" placeholder="Your Email" type="email" value=""/>
-                </div>
 
 
-                <div class="collectEmailFormGroup">
-                    <div class="label-error-group">
-                        <span class="field-validation-valid validation-message" data-valmsg-for="FirstName"
-                              data-valmsg-replace="true"></span>
+                    <div class="collectEmailFormGroup">
+                        <div class="label-error-group">
+                            <span class="field-validation-valid validation-message" data-valmsg-for="FirstName"
+                                  data-valmsg-replace="true"></span>
+                        </div>
+                        <input class="form-control form-control-sm" id="FirstName" maxlength="255" name="FirstName" placeholder="Your Full Name" type="text"
+                               value=""/>
                     </div>
-                    <input id="FirstName" maxlength="255" name="FirstName" placeholder="Your Full Name" type="text"
-                           value=""/>
-                </div>
 
-                <div class="collectEmailFormGroup collectEmailFormActions">
-                    <button type="submit" class="newsletter-btn">Submit</button>
-                </div>
+                    <div class="collectEmailFormGroup collectEmailFormActions">
+                        <button type="submit" class="newsletter-btn">Submit</button>
+                    </div>
 
-            </form>
-        </div>&ndash;&gt;
+                </form>
+            </div>
 
-    </div>-->
+        </div>
     <div class="footer-inner ml-5">
         <div class="footer-open">
             <hr class="phone-hr">
@@ -112,13 +127,14 @@
 <div class="trademark d-flex bd-highlight">
     <div class="p-2 flex-grow-1 bd-highlight">&copy <?php echo date("Y"); ?> Excel Jewellers - All Rights Reserved</div>
     <div class="payment-container ml-3">
-        <div class="payment-icon ml-3"><img alt="Visa Icon Card Excel Jewellers"
+        <div class="payment-icon " style="padding-right: 10px;"><img alt="Visa Icon Card Excel Jewellers"
                                             src="<?php echo e(asset('storage/image/icons/visa.png')); ?>"></div>
-        <div class="payment-icon ml-3"><img alt="Mastercard Icon Card Excel Jewellers"
+        <div class="payment-icon" style="padding-right: 10px;">
+            <img alt="Mastercard Icon Card Excel Jewellers"
                                             src="<?php echo e(asset('storage/image/icons/mastercard.png')); ?>"></div>
-        <div class="payment-icon ml-3"><img alt="Paypal Icon Card Excel Jewellers"
+        <div class="payment-icon" style="padding-right: 10px;"><img alt="Paypal Icon Card Excel Jewellers"
                                             src="<?php echo e(asset('storage/image/icons/paypal.png')); ?>"></div>
-        <div class="payment-icon ml-3"><img alt="American Eagle Icon Card Excel Jewellers"
+        <div class="payment-icon "><img alt="American Eagle Icon Card Excel Jewellers"
                                             src="<?php echo e(asset('storage/image/icons/ae.png')); ?>"></div>
     </div>
 </div>
